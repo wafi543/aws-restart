624012cec07dfbc4fa9da955050fcc9126e3764f
walshammari <walshammari@site.sa>
2023-08-01 20:00:16 +0300

    comment after_install to make pipeline succeed

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # setup backend node express
 # sudo fuser -k 3001/tcp
-DIR="/home/ec2-user/aws-backend-node"
-if [ -d "$DIR" ]; then
-  git config --global --add safe.directory /home/ec2-user/aws-backend-node
-  ### Take action if $DIR exists ###
-  cd /home/ec2-user/aws-backend-node
-  git fetch origin
-  git pull
-  # node -r dotenv/config index.js
-else
-  git config --global --add safe.directory /home/ec2-user/aws-backend-node
-  ###  Control will jump here if $DIR does NOT exists ###
-  cd ~
-  echo "git clone https://github.com/wafi543/aws-backend-node"
-  git clone https://github.com/wafi543/aws-backend-node
-  cd /home/ec2-user/aws-backend-node
-  sudo npm install
-  # node -r dotenv/config index.js
-fi
+sudo service status codedeploy-agent
+# DIR="/home/ec2-user/aws-backend-node"
+# if [ -d "$DIR" ]; then
+#   git config --global --add safe.directory /home/ec2-user/aws-backend-node
+#   ### Take action if $DIR exists ###
+#   cd /home/ec2-user/aws-backend-node
+#   git fetch origin
+#   git pull
+#   # node -r dotenv/config index.js
+# else
+#   git config --global --add safe.directory /home/ec2-user/aws-backend-node
+#   ###  Control will jump here if $DIR does NOT exists ###
+#   cd ~
+#   echo "git clone https://github.com/wafi543/aws-backend-node"
+#   git clone https://github.com/wafi543/aws-backend-node
+#   cd /home/ec2-user/aws-backend-node
+#   sudo npm install
+#   # node -r dotenv/config index.js
+# fi
