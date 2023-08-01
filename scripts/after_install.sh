2cbe9a628d037338bc15e8c3170976d78f86a90d
walshammari <walshammari@site.sa>
2023-08-01 19:58:47 +0300

    fix bug

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,12 +3,14 @@
 # sudo fuser -k 3001/tcp
 DIR="/home/ec2-user/aws-backend-node"
 if [ -d "$DIR" ]; then
+  git config --global --add safe.directory /home/ec2-user/aws-backend-node
   ### Take action if $DIR exists ###
   cd /home/ec2-user/aws-backend-node
   git fetch origin
   git pull
   # node -r dotenv/config index.js
 else
+  git config --global --add safe.directory /home/ec2-user/aws-backend-node
   ###  Control will jump here if $DIR does NOT exists ###
   cd ~
   echo "git clone https://github.com/wafi543/aws-backend-node"
